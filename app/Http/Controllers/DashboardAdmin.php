924883ab1f35b31bf289b24febbb979b60d85f80
231111004 <231111004@mhs.stiki.ac.id>
2025-09-06 14:22:39 +0700

    ganti template

--- app/Http/Controllers/DashboardAdmin.php
+++ app/Http/Controllers/DashboardAdmin.php
@@ -3,21 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class DashboardAdmin extends Controller
 {
-    
-    public function index(){
-
+    public function index()
+    {
         $data['title'] = "Dashboard";
-
         $data['NAMA_DEPAN'] = "ADMIN";
 
-        return 
-        view('Layouts.header',$data).
-        view('Layouts.sidebar',$data).
-        view('admin.dashboard',$data).
-        view('Layouts.footer');
+        return
+            view('Layouts.header', $data) .
+            view('admin.dashboard', $data) .
+            view('Layouts.footer', $data);
     }
 }
+
--- resources/views/Layouts/footer.blade.php
+++ resources/views/Layouts/footer.blade.php
@@ -1,24 +1,16 @@
-<!-- resources/views/admin_page/footer.blade.php -->
-<!--! ================================================================ !-->
-<!--! Footer Script !-->
-<!--! ================================================================ !-->
 
-<!--! BEGIN: Vendors JS !-->
-<script src="{{ asset('assets/admin/vendors/js/vendors.min.js') }}"></script>
-<!-- vendors.min.js {always must need to be top} -->
-<script src="{{ asset('assets/admin/vendors/js/daterangepicker.min.js') }}"></script>
-<script src="{{ asset('assets/admin/vendors/js/apexcharts.min.js') }}"></script>
-<script src="{{ asset('assets/admin/vendors/js/circle-progress.min.js') }}"></script>
 <!--! END: Vendors JS !-->
-
 <!--! BEGIN: Apps Init  !-->
-<script src="{{ asset('assets/admin/js/common-init.min.js') }}"></script>
-<script src="{{ asset('assets/admin/js/dashboard-init.min.js') }}"></script>
+<script src="{{ asset('assets_admin/js/common-init.min.js') }}"></script>
+<script src="{{ asset('assets_admin/js/dashboard-init.min.js') }}"></script>
 <!--! END: Apps Init !-->
-
 <!--! BEGIN: Theme Customizer  !-->
-<script src="{{ asset('assets/admin/js/theme-customizer-init.min.js') }}"></script>
+<script src="{{ asset('assets_admin/js/theme-customizer-init.min.js') }}"></script>
 <!--! END: Theme Customizer !-->
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
 </body>
+
 </html>
